8f0d0f302bf1c6b738bdd00aa5d8840fce31404b
Ильченко Арсений <eshsupman@gmail.com>
2024-04-14 16:59:31 +0300

    create empty skrepysh backend

---
+++ skrepysh-backend/src/test/kotlin/com/skrepysh/skrepyshbackend/SkrepyshBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skrepysh.skrepyshbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkrepyshBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
