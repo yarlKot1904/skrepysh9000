7fa103c73e4d7747d521cfaeb365892da9796539
Ильченко Арсений <eshsupman@gmail.com>
2024-04-14 16:59:31 +0300

    create empty skrepysh backend

---
+++ skrepysh-backend/src/main/kotlin/com/skrepysh/skrepyshbackend/SkrepyshBackendApplication.kt
@@ -0,0 +1,12 @@
+package com.skrepysh.skrepyshbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkrepyshBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkrepyshBackendApplication>(*args)
+
+}
