62b635dcda887bb859477cdb209d8eef79696a2e
Шевцов Антон <n3ktar56@gmail.com>
2024-05-11 21:19:48 +0300

    Added node_exporter download

--- image/ubuntu/scripts/install-packages.sh
+++ image/ubuntu/scripts/install-packages.sh
@@ -7,3 +7,12 @@ apt-cache policy docker-ce
 apt install docker-ce
 systemctl status docker
 sudo usermod -aG docker ${USER}
+
+apt install tar wget
+wget -P /tmp https://github.com/prometheus/node_exporter/releases/download/v1.8.0/node_exporter-1.8.0.linux-amd64.tar.gz
+tar -C /usr/local/bin -xvf /tmp/node_exporter-1.8.0.linux-amd64.tar.gz
+mv /usr/local/bin/node_exporter-1.8.0.linux-amd64/node_exporter /usr/local/bin
+rm -r /usr/local/bin/node_exporter-1.8.0.linux-amd64
+rm /tmp/node_exporter-1.8.0.linux-amd64.tar.gz
+
+
