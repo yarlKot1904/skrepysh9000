ac454b8c916f4cd830f9528f1706ed1be94ec847
Ильченко Арсений <eshsupman@gmail.com>
2024-05-11 16:55:53 +0300

    created delete controller

---
+++ skrepysh-backend/src/main/kotlin/com/skrepysh/skrepyshbackend/controllers/DeleteController.kt
@@ -0,0 +1,39 @@
+package com.skrepysh.skrepyshbackend.controllers
+
+import com.skrepysh.skrepyshbackend.database.DatabaseVM
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+
+data class DeleteRequestBody(var ip: String)
+
+@RestController
+class DeleteController(@Autowired private val database: DatabaseVM) {
+    @Autowired
+    private val context: HttpServletRequest? = null
+
+    val log: Logger = LoggerFactory.getLogger(DeleteController::class.java)
+
+    @PostMapping("/delete")
+    @ResponseBody
+    fun init(@RequestBody request: DeleteRequestBody): ResponseEntity<String> {
+        log.info("${context!!.method} request /delete: $request")
+        try {
+            database.deleteVM(request.ip)
+            return ResponseEntity<String>(HttpStatus.OK)
+        } catch (e: Exception) {
+            log.error("Error deleting vm to database: $request")
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "error: ${e.message}")
+        }
+
+    }
+}
