f4d0d142c390e30d56a941352897c403178567c3
Cyrill Molotov <molotov.cyrill@yandex.ru>
2024-05-15 05:12:14 +0300

    update ubuntu image

--- image/ubuntu/scripts/provision.sh
+++ image/ubuntu/scripts/provision.sh
@@ -4,17 +4,38 @@ mkdir -p /etc/skrepysh/
 cat <<EOF >> /etc/skrepysh/config.yaml
 log:
   level: DEBUG
+
+server-port: 48934
+
+node-exporter:
+  host: localhost
+  port: 9100
+
 EOF
 
 cat <<EOF > $wd/etc/systemd/system/skrepysh-agent.service
 [Unit]
 Description=Skrepysh Agent
 After=docker.service
-Requires=docker.service
 
 [Service]
 Type=simple
 ExecStart=/usr/local/bin/skrepysh-agent serve
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat <<EOF > $wd/etc/systemd/system/node-exporter.service
+[Unit]
+Description=Node Exporter
+After=docker.service
+Requires=skrepysh-agent.service
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
 Restart=always
 
 [Install]
